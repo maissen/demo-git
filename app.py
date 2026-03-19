cea7f29c708031215b2db0b09ff372fbc3235ad5
Maissen <maissen.developer500@gmail.com>
2026-03-19 01:33:51 +0100

    feat: something

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 print("Hello World!")
 name = "Maissen Belgacem"
 age = 23
+
+
+def greeting(name: str, age: int):
+    print("Hello " + name + " " + str(age))
+
+
+greeting(name, age)
+#this is a comment
+#hello
