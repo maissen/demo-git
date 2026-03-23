eedaeb9f3ace6f613d176f4a014f5fae2398b225
Maissen <maissen.developer500@gmail.com>
2026-03-23 11:10:51 +0100

    feat: greet with default param and log

--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
-const greet = (name) => `Hello ${name}`;
+const greet = (name = "stranger") => {
+  console.log("greeting user...");
+  return `Hello ${name}`;
+}
 
 const farewell = (name) => `Bye ${name}`;
