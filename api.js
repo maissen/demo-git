81bf9b4a3b0b5fc15e48031b1d107d1ee9c25153
Bob <bob@gmail.com>
2026-03-23 11:40:07 +0100

    refactor: add json parsing, remove logout

--- api.js
+++ api.js
@@ -1,11 +1,8 @@
 function fetchUser(id) {
-  return fetch("/api/users/" + id);
+  return fetch("/api/users/" + id)
+    .then(r => r.json());
 }
 
 function fetchPosts() {
   return fetch("/api/posts");
-}
-
-function logout() {
-  localStorage.clear();
 }
